5c44e18214c787975afc5db4b502390a31886a12
hozay wut <hozaywut@org.org>
2023-11-14 11:56:38 -0500

    adding some type safety

--- src/interface.ts
+++ src/interface.ts
@@ -1,18 +1,37 @@
-const actionButtonNodes = () => {
-  const actionBtnsContainer =
-    document.querySelectorAll('[data-icon="play"]')[0].parentElement
-      .parentElement;
+type Coords = {
+  x: number;
+  y: number;
+};
+
+type ActionButtonNodes = {
+  play: HTMLElement;
+  edit: HTMLElement;
+  delete: HTMLElement;
+};
+
+const actionButtonNodes = (): ActionButtonNodes => {
+  const actionBtnsContainer = document.getElementById(
+    "action-buttons-container"
+  );
+
+  if (!actionBtnsContainer) {
+    throw new Error("Action buttons container not found");
+  }
 
   return {
-    play: actionBtnsContainer.childNodes[0],
-    edit: actionBtnsContainer.childNodes[1],
-    delete: actionBtnsContainer.childNodes[2],
+    play: actionBtnsContainer.children[0] as HTMLElement,
+    edit: actionBtnsContainer.children[1] as HTMLElement,
+    delete: actionBtnsContainer.children[2] as HTMLElement,
   };
 };
 
-const renderOption = (label: string, sibling: any, simulatedNode: any) => {
+const renderOption = (
+  label: string,
+  sibling: HTMLElement,
+  simulatedNode: HTMLElement
+) => {
   const option = document.createElement("a");
-  option.id = label + "-option";
+  option.id = `${label.toLowerCase()}-option`;
   option.classList.add(
     "bg-secondary",
     "text-white",
@@ -29,65 +48,61 @@ const renderOption = (label: string, sibling: any, simulatedNode: any) => {
 };
 
 const openContextMenu = (
-  e: any,
-  coords: {
-    x: number;
-    y: number;
-  } = { x: e.clientX, y: e.clientY }
+  e: MouseEvent,
+  coords: Coords = { x: e.clientX, y: e.clientY }
 ) => {
-  e.preventDefault(); // Get mouse position
-  let mouseX = e.clientX;
-  let mouseY = e.clientY;
-  if (coords && coords.x && coords.y) {
-    mouseX = coords.x;
-    mouseY = coords.y;
-  }
+  e.preventDefault();
+
+  const mouseX = coords.x;
+  const mouseY = coords.y;
   const moreMenu = document.getElementById("more-menu");
+
+  if (!moreMenu) {
+    throw new Error("More menu not found");
+  }
 
   const parent = moreMenu.parentElement;
 
-  // the sibling holds the context menu options
-  const sibling = moreMenu.nextElementSibling;
-
-  if (!parent.classList.contains("show")) {
+  if (parent && !parent.classList.contains("show")) {
     moreMenu.click();
   }
 
-  parent.classList.add("context-menu-container");
-  parent.style.position = "fixed";
-  parent.style.left = mouseX + 5 + "px";
-  parent.style.top = mouseY - 10 + "px";
-  parent.style.display = "block";
-  parent.style.zIndex = "1000";
+  if (parent) {
+    parent.classList.add("context-menu-container");
+    parent.style.position = "fixed";
+    parent.style.left = mouseX + 5 + "px";
+    parent.style.top = mouseY - 10 + "px";
+    parent.style.display = "block";
+    parent.style.zIndex = "1000";
+  }
 
   const checkedCheckboxes = document.querySelectorAll(
     ".row.justify-content-center input:checked"
   );
 
-  // lets set all the options to order: 2;
+  const sibling = moreMenu.nextElementSibling as HTMLElement;
+
   sibling.childNodes.forEach((child) => {
-    child.style.order = "2";
+    if (child instanceof HTMLElement) {
+      child.style.order = "2";
+    }
   });
 
-  // if we have at least one checked checkbox, we will render the (play,edit,delete) options in the context menu
   if (checkedCheckboxes.length > 0) {
-    // lets add the context menu options to sibling
-    // if it doenst esst #${label}-option
-    !document.getElementById("Play-option") &&
+    !document.getElementById("play-option") &&
       renderOption("Play", sibling, actionButtonNodes().play);
-    !document.getElementById("Edit-option") &&
+    !document.getElementById("edit-option") &&
       renderOption("Edit", sibling, actionButtonNodes().edit);
-    !document.getElementById("Delete-option") &&
+    !document.getElementById("delete-option") &&
       renderOption("Delete", sibling, actionButtonNodes().delete);
   } else {
-    // lets remove the context menu options from sibling
-    document.getElementById("Play-option")?.remove();
-    document.getElementById("Edit-option")?.remove();
-    document.getElementById("Delete-option")?.remove();
+    document.getElementById("play-option")?.remove();
+    document.getElementById("edit-option")?.remove();
+    document.getElementById("delete-option")?.remove();
   }
 };
 
-const handleRightClick = (e: any) => openContextMenu(e);
+const handleRightClick = (e: MouseEvent) => openContextMenu(e);
 
 document.addEventListener("contextmenu", openContextMenu);
 
@@ -97,9 +112,9 @@ document.addEventListener("mousemove", (event) => {
   window.lastMousePosition = { x: mouseX, y: mouseY };
 });
 
-// open the context menu with cmd + . or ctrl + .
 document.addEventListener("keydown", (e) => {
   if ((e.ctrlKey && e.key === ".") || (e.metaKey && e.key === ".")) {
     openContextMenu(e, window.lastMousePosition);
   }
 });
+alert("4");
